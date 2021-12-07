f2f04c32776354257c69a3ecdc4fe717702a3dc8
Nate Silva <nate.s.silva@gmail.com>
2021-12-06 22:22:51 -0700

    implement day 7

---
+++ day7/part2.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const data = fs.readFileSync("./input.txt").toString()
+
+const values = data.split(',').map(Number)
+
+const min = Math.min(...values);
+const max = Math.max(...values);
+
+let minFuel;
+for (let i = min; i <= max; i++) {
+    const fuel = values.map(v =>
+        Math.floor(
+            (Math.abs(v - i) + 1) / 2 * (Math.abs(v - i))
+        )
+    ).reduce((a, b) => a + b, 0);
+    if (minFuel == null || fuel < minFuel) {
+        minFuel = fuel
+    }
+}
+console.log(minFuel)
